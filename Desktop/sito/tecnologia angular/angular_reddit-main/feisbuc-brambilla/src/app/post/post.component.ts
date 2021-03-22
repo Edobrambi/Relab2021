8fdb9e99d7e8e366a72ea17b98bfec0769e9cd09
edoardo <edoardobrambilla03@gmail.com>
2021-03-22 10:50:54 +0100

    punto 6

--- Desktop/sito/tecnologia angular/angular_reddit-main/feisbuc-brambilla/src/app/post/post.component.ts
+++ Desktop/sito/tecnologia angular/angular_reddit-main/feisbuc-brambilla/src/app/post/post.component.ts
@@ -9,11 +9,15 @@ export class PostComponent implements OnInit {
   @HostBinding('attr.class') cssClass = 'card mt-4';
   name: string;
   text: string;
+  
   @Input() post: Post;
   constructor() {
-    this.post = new Post('gino', 'tanto va la gatta al lardo che ci lascia lo zampino');
+    this.post = new Post('gino', 'tanto va la gatta al lardo che ci lascia lo zampino',10);
    }
-
+   addLike():boolean{
+     this.post.addLike();
+     return false;
+   }
   ngOnInit(): void {
   }
 
--- Desktop/sito/tecnologia angular/angular_reddit-main/feisbuc-brambilla/src/app/post/post.model.ts
+++ Desktop/sito/tecnologia angular/angular_reddit-main/feisbuc-brambilla/src/app/post/post.model.ts
@@ -1,11 +1,17 @@
 export class Post {
     name: string;
     text: string;
-    
-    constructor(name: string, text: string) { 
+    like: number;
+    constructor(name: string, text: string, like?: number ) { 
         this.name = name;
         this.text = text; 
-    
+        this.like = like || 0;
     }
+    addLike(): void {
+        this.like += 1;
+      }
+    
+     
+
     
 }
