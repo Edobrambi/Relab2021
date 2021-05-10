b28ff9661123eba69470acf338170d2d582f1b2b
edoardo <edoardobrambilla03@gmail.com>
2021-05-10 09:47:12 +0200

    punto 3

--- Desktop/sito/tecnologia angular/angular_reddit-main/brambilla-booking/src/app/app.component.ts
+++ Desktop/sito/tecnologia angular/angular_reddit-main/brambilla-booking/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import{ HttpClient } from '@angular/common/http';
+import { Hotel } from './models/hotel.model';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'brambilla-booking';
+export class AppComponent implements OnInit {
+  obsHotel: Observable <Hotel[]>;
+  listaHotels: Hotel[];
+  constructor(private http: HttpClient){}
+  ngOnInit(): void {
+    this.obsHotel = this.http.get<Hotel[]>('https://my-json-server.typicode.com/malizia-g/hotel/short-booking');
+    this.obsHotel.subscribe(this.getHotels);
+ 
+  }
+
+  getHotels = (data: Hotel[]) =>{
+    this.listaHotels = data;
+    
+    console.log(this.listaHotels);
+  }
 }
